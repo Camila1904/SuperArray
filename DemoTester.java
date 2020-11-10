06c2cb2bd230fff5096d8ad487fba64c7692597f
Camila Contreras <ccontreras20@stuy.edu>
2020-11-10 11:08:44 -0800

    zip works

--- DemoTester.java
+++ DemoTester.java
@@ -14,7 +14,16 @@ public class DemoTester {
     tester1.add("3");
     tester1.add("2");
     System.out.println(tester1);
+    SuperArray tester2 = new SuperArray();
+    tester2.add("0");
+    tester2.add("1");
+    tester2.add("4");
+    tester2.add("6");
+    tester2.add("2");
+    System.out.println(tester2);
     Demo.removeDuplicates(tester1);
     System.out.println(tester1);
+    System.out.println(Demo.findOverlap(tester1, tester2));
+    System.out.println(Demo.zip(tester1,tester2));
   }
 }
