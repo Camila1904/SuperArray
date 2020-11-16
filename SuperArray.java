7a21657bbd9d91413d51d7c0ebea601b621666f7
Camila Contreras <ccontreras20@stuy.edu>
2020-11-16 10:38:54 -0800

    both errror methods work

--- SuperArray.java
+++ SuperArray.java
@@ -6,6 +6,9 @@ public class SuperArray {
     size = 0;
   }
   public SuperArray(int startingCapacity) {
+    if (startingCapacity< 0) {
+      throw new IllegalArgumentException("Starting Capacity" + startingCapacity + "cannot be negative");
+    }
     data = new String[startingCapacity];
     size = 0;
   }
